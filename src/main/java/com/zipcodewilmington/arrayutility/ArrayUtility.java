c4484629882e0412458aa036b7c5510df9864575
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-04-06 00:13:54 -0400

    Stream mostCommonFromMerge

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,79 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    public ArrayList<T> inputArray = new ArrayList<>();
+
+    public T[] input;
+    public ArrayUtility(T[] inputArray) {
+        this.inputArray.addAll(Arrays.asList(inputArray));
+        this.input = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        this.inputArray.addAll(Arrays.asList(arrayToMerge));
+        return countDuplicates(valueToEvaluate);
+    }
+    private Integer countDuplicates(T item){
+        int count = 0;
+        for(T element : this.input){
+            if(item.equals(element)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        //this.inputArray.addAll(Arrays.asList(arrayToMerge));
+        //T[] newArray = inputArray.toArray(new T[0]);
+
+//       Stream.concat(inputArray.stream(), Arrays.stream(arrayToMerge))
+//                .toArray(size -> (T[]) Array.newInstance(inputArray.getClass().getComponentType(), size));
+        Map<T, Long> occurences =
+
+                Arrays.stream(arrayToMerge)
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+                Arrays.stream(inputArray.toArray())
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+
+        return occurences
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+
+
+
+    }
+
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return (int)inputArray.stream()
+                .filter(element -> valueToEvaluate == element)
+                .count();
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        List<T> processedList = inputArray
+                .stream()
+                .filter(element -> valueToRemove != element)
+                .collect(Collectors.toList());
+        //T[] processedArray = new
+        return (T[]) processedList.toArray(new Object[processedList.size()]);
+    }
 }
